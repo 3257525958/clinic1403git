2a098f3a4b6c64deb5aa8028f46613feb6ce1b30
Asus <ma4301511@gmail.com>
2024-03-22 01:57:07 +0330

    clinic1403-8

--- clinic1403/settings.py
+++ clinic1403/settings.py
@@ -160,8 +160,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/app/public/static/'
-MEDIA_URL = '/app/public/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT= os.path.join(BASE_DIR,"static")
 MEDIA_ROOT= os.path.join(BASE_DIR,"media")
