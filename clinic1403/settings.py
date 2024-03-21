f2bbfc8e2d8f7445804cb031c697c8bbd94f89f3
Asus <ma4301511@gmail.com>
2024-03-21 13:49:45 +0330

    new2

--- clinic1403/settings.py
+++ clinic1403/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-2g&%dn06my3m55@+8_9%po(ro$qavn=d!7px(#zkqk6xwkz&^*
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# ALLOWED_HOSTS = ['http://drmahdiasadpour.ir','drmahdiasadpour.ir','www.drmahdiasadpour.ir']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://drmahdiasadpour.ir','drmahdiasadpour.ir','www.drmahdiasadpour.ir']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -108,22 +108,23 @@ WSGI_APPLICATION = 'clinic1403.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'clinicbajff_db',
-#         'USER': 'root',
-#         'PASSWORD': 'jz41nnvhx9jwmeg',
-#         'HOST': 'clinicbank-iwd-service',
-#
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clinic14jfr_db',
+        'USER': 'root',
+        'PASSWORD': 'z8qUknZdEu9yO8NlPpEb',
+        'HOST': 'clinic1403data-god-service',
+
+    }
+}
 
 
 # Password validation
