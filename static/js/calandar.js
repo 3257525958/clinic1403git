96e68609e888b3057e8887335e2ea5454c0a1860
Asus <ma4301511@gmail.com>
2024-03-23 15:45:01 +0330

    clinic1403-16

--- static/js/calandar.js
+++ static/js/calandar.js
@@ -14,7 +14,7 @@
  function move() {
     var elem = document.getElementById("myBar");
     var width = 0;
-    var id = setInterval(frame, 20);
+    var id = setInterval(frame, 40);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
