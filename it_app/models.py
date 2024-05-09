95dbbd5dcdd47cec4b8de20f5e442378ff815aa9
Asus <ma4301511@gmail.com>
2024-05-10 01:22:42 +0330

    hesab11.mesage2

--- it_app/models.py
+++ it_app/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 
 class mesaagetextmodel(models.Model):
-    name = models.CharField(max_length=100)
-    mesaagetext = models.TextField (max_length=900000)
+    name = models.CharField(max_length=100,default="0")
+    mesaagetext = models.TextField (max_length=900000,default="0")
     def __str__(self):
         return f"{self.name}"
 
 
 class mesaagecuntermodel(models.Model):
-    name = models.CharField(max_length=100)
-    date = models.CharField(max_length=100)
-    sender = models.CharField(max_length=100)
-    cunt = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,default="0")
+    dateyear = models.CharField(max_length=100,default="0")
+    datemuonth = models.CharField(max_length=100,default="0")
+    dateday = models.CharField(max_length=100,default="0")
+    dateweek = models.CharField(max_length=100,default="0")
+    sender = models.CharField(max_length=100,default="0")
+    cunt = models.CharField(max_length=100,default="0")
     def __str__(self):
         return f"{self.name}"
--- it_app/views.py
+++ it_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from it_app.models import mesaagetextmodel,mesaagecuntermodel
+import datetime
+from cantact_app.views import stry,strd,strb,stra
 def sendmesaage(request):
     return render(request,'mesage_send.html')
 def savemesaage(request):
-
+    savebutton = request.POST.get('savebutton')
+    mesaagename = request.POST.get('mesaagename')
+    mesaagetext = request.POST.get('mesaagetext')
+    if (savebutton == 'accept') and (mesaagename != '') and (mesaagetext != '') and (mesaagename != None) and (mesaagetext != None):
+        t = datetime.datetime.now()
+        p = request.user.username
+        mesaagetextmodel.objects.create(name=mesaagename,dateyear=stry(t),datemuonth=strb(t),dateday=strd(t),dateweek=stra(t),sender=p)
     return render(request,'mesaage_save.html')
