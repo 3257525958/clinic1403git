4cbd05a23e76f7bfc016180edf8778f95a4eee7c
Asus <ma4301511@gmail.com>
2024-05-22 01:03:09 +0330

    hesab11.reserv2

--- clinic1403/urls.py
+++ clinic1403/urls.py
@@ -35,4 +35,4 @@ urlpatterns = [
 
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
