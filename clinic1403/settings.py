c827653e6870f493ff1c183f3e6fcc8f07c3764c
Asus <ma4301511@gmail.com>
2024-06-02 02:33:07 +0330

    home img add or delet2

--- clinic1403/settings.py
+++ clinic1403/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cleanup.apps.CleanupConfig',
+    # 'django_cleanup.apps.CleanupConfig',
     'home_app.apps.HomeAppConfig',
     'cantact_app.apps.CantactAppConfig',
     'jalali_date',
