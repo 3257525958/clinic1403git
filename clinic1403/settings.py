417e9ba9dc518d5ac3ea07f32486550441d567c9
Asus <ma4301511@gmail.com>
2024-06-02 22:30:26 +0330

    home img add or delet3

--- clinic1403/settings.py
+++ clinic1403/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     # 'peyment_app.apps.PeymentAppConfig'
     'cash_app.apps.CashAppConfig',
     'it_app.apps.ItAppConfig',
-    'django_cleanup.apps.CleanupConfig',
+    # 'django_cleanup.apps.CleanupConfig',
 ]
 JALALI_DATE_DEFAULTS = {
    'Strftime': {
