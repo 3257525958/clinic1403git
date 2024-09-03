cc69a7b5d79829d0bfe8a3210aa00dede551368f
Asus <ma4301511@gmail.com>
2024-09-04 00:40:13 +0330

    11

--- home_app/views.py
+++ home_app/views.py
@@ -14,23 +14,23 @@ profilestatus =['']
 loglevel = ['']
 def home(request):
     btndate = request.POST.get('btndate')
-    if btndate == 'accept':
-        try:
-            api = KavenegarAPI(
-                '527064632B7931304866497A5376334B6B506734634E65422F627346514F59596C767475564D32656E61553D')
-            params = {
-                'sender': '9982003178',  # optional
-                'receptor': '09122852099',  # multiple mobile number, split by comma
-                'message':" سلام",
-            }
-            response = api.sms_send(params)
-            # return render(request, 'code_cantact.html')
-        except APIException as e:
-            m = 'tellerror'
-            return render(request, 'closecash.html', context={'melicod_etebar': m})
-        except HTTPException as e:
-            m = 'neterror'
-            return render(request, 'closecash.html', context={'melicod_etebar': m}, )
+    # if btndate == 'accept':
+    #     try:
+    #         api = KavenegarAPI(
+    #             '527064632B7931304866497A5376334B6B506734634E65422F627346514F59596C767475564D32656E61553D')
+    #         params = {
+    #             'sender': '9982003178',  # optional
+    #             'receptor': '09122852099',  # multiple mobile number, split by comma
+    #             'message':" سلام",
+    #         }
+    #         response = api.sms_send(params)
+    #         # return render(request, 'code_cantact.html')
+    #     except APIException as e:
+    #         m = 'tellerror'
+    #         return render(request, 'closecash.html', context={'melicod_etebar': m})
+    #     except HTTPException as e:
+    #         m = 'neterror'
+    #         return render(request, 'closecash.html', context={'melicod_etebar': m}, )
 
     r = request.GET.get("r")
     loglevel[0] = [""]
