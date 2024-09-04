909f1ea8382593eb929f6df5f54e718a4c4dd52b
Asus <ma4301511@gmail.com>
2024-09-04 12:25:23 +0330

    11

--- cash_app/models.py
+++ cash_app/models.py
@@ -9,17 +9,16 @@ class bankmodel(models.Model):
     def __str__(self):
         return f"{self.onvan}"
 class castmodel(models.Model):
-    idf = models.CharField(max_length=20,default="0",null=True)
+    # idf = models.CharField(max_length=20,default="0",null=True)
     melicodvarizande = models.CharField(max_length=10,default='0',null=True)
-    dateshamsi = models.CharField(max_length=100,default='0',null=True)
-    datemiladi = models.CharField(max_length=100,default='0',null=True)
+    # dateshamsi = models.CharField(max_length=100,default='0',null=True)
+    # datemiladi = models.CharField(max_length=100,default='0',null=True)
     filenumber = models.CharField(max_length=100,default='0',null=True)
-    cashmethodname = models.CharField(max_length=100,default='0',null=True)
-    cashmethodid = models.CharField(max_length=100,default='0',null=True)
-    melicodeoperatore = models.CharField(max_length=100,default='0',null=True)
-    mablagh = models.CharField(max_length=100,default='0',blank=True,null=True)
-    dateshamsieditor = models.CharField(max_length=100,default='0',null=True)
-
+    # cashmethodname = models.CharField(max_length=100,default='0',null=True)
+    # cashmethodid = models.CharField(max_length=100,default='0',null=True)
+    # melicodeoperatore = models.CharField(max_length=100,default='0',null=True)
+    # mablagh = models.CharField(max_length=100,default='0',blank=True,null=True)
+    # dateshamsieditor = models.CharField(max_length=100,default='0',null=True)
 
     def __str__(self):
         return f"{self.filenumber}"
