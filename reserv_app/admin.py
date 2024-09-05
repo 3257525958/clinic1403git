2ac7568cbd2f1a2fef7c2709df424dff06a5d5d2
Asus <ma4301511@gmail.com>
2024-09-05 23:53:26 +0330

    ended4pppp

--- reserv_app/admin.py
+++ reserv_app/admin.py
@@ -1,11 +1,11 @@
-# from django.contrib import admin
-#
-# from reserv_app.models import reservemodel,leavemodel,reservemodeltest,neursemodel,filepage1model,searchmodeltest,fpeseshktestmodel
-#
-# admin.site.register(reservemodel)
-# admin.site.register(reservemodeltest)
-# admin.site.register(leavemodel)
-# admin.site.register(neursemodel)
-# admin.site.register(filepage1model)
-# admin.site.register(searchmodeltest)
-# admin.site.register(fpeseshktestmodel)
+from django.contrib import admin
+
+from reserv_app.models import reservemodel,leavemodel,reservemodeltest,neursemodel,filepage1model,searchmodeltest,fpeseshktestmodel
+
+admin.site.register(reservemodel)
+admin.site.register(reservemodeltest)
+admin.site.register(leavemodel)
+admin.site.register(neursemodel)
+admin.site.register(filepage1model)
+admin.site.register(searchmodeltest)
+admin.site.register(fpeseshktestmodel)
