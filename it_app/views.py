19cd3c765b7ce14bc08bd41dba0b252b5df6658c
Asus <ma4301511@gmail.com>
2024-09-13 03:18:49 +0330

    tavalod

--- it_app/views.py
+++ it_app/views.py
@@ -147,29 +147,31 @@ def itdeletcontrol(request):
 
 def tim():
     def sen():
-        users = accuntmodel.objects.all()
-        t = datetime.datetime.now()
-        for user in users:
-            if (user.mountb == strb(t)) and (user.dayb == strd(t)):
-                name = user.firstname + ' ' + user.lastname
-                smstext = 'سلام' + ' ' + name + ' ' + 'عزیز' + '\n' + "زادروز تولدت مبارک"+'\n'+''+'\n'+'\n'+'\n'+'لغو 11'
-                try:
-                    api = KavenegarAPI(
-                        '527064632B7931304866497A5376334B6B506734634E65422F627346514F59596C767475564D32656E61553D')
-                    params = {
-                        'sender': '9982003178',  # optional
-                        'receptor': "09122852099",  # multiple mobile number, split by comma
-                        'message': smstext,
-                    }
-                    response = api.sms_send(params)
-                    # return render(request, 'code_cantact.html')
-                except APIException as e:
-                    m = 'tellerror'
-                except HTTPException as e:
-                    m = 'neterror'
+        print("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+        # users = accuntmodel.objects.all()
+        # t = datetime.datetime.now()
+        # for user in users:
+        #     if (user.mountb == strb(t)) and (user.dayb == strd(t)):
+        #         name = user.firstname + ' ' + user.lastname
+        #         smstext = 'سلام' + ' ' + name + ' ' + 'عزیز' + '\n' + "زادروز تولدت مبارک"+'\n'+'مفتخریم تا با هدیه ای هر چند کوچک در این مناسبت فرخنده در کنار شما باشیم'+'\n'+'برای دریافت هدیه عدد 3 را به همین سامانه ارسال فرمایید'+'\n'+'مطب دکتر اسدپور'+'\n'+'لغو 11'
+        #         try:
+        #             api = KavenegarAPI(
+        #                 '527064632B7931304866497A5376334B6B506734634E65422F627346514F59596C767475564D32656E61553D')
+        #             params = {
+        #                 'sender': '9982003178',  # optional
+        #                 'receptor': "09122852099",  # multiple mobile number, split by comma
+        #                 'message': smstext,
+        #             }
+        #             response = api.sms_send(params)
+        #             # return render(request, 'code_cantact.html')
+        #         except APIException as e:
+        #             m = 'tellerror'
+        #         except HTTPException as e:
+        #             m = 'neterror'
 
     # Schedule the message to be sent at midnight
-    schedule.every().day.at("23:40").do(sen)
+    # schedule.every().day.at("20:30").do(sen)
+    schedule.every(10).seconds.do(sen)
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -177,5 +179,3 @@ def tim():
 
 t = Thread(target=tim)
 t.start()
-print("aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-print(datetime.datetime.now())
