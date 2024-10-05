8ed02c77908f2936a9e8605f5e07d78af0182cd6
Asus <ma4301511@gmail.com>
2024-10-06 00:57:08 +0330

    node test

--- static/js/watsapp.js
+++ static/js/watsapp.js
@@ -1,24 +1,41 @@
-            let radioList = document.querySelectorAll('input');
-            let underLine = document.querySelector('.underline');
-            let content = document.querySelector('.content');
+//                 document.getElementById("textsend").click();
+//                 document.getElementById("textsend").value = document.getElementById("texttextttt").innerHTML;
+//                 document.getElementById("textsend").click();
+// function  cklik(){
+//                 document.getElementById("textsend").click();
+//                 document.getElementById("check").click();
+//                 document.getElementById("textsend").click();
+// }
+// function startTimer() {
+//     document.getElementById("textsend").click();
+//     set_inteval = setInterval( 'cklik()', 6000 );
+// }
+// startTimer();
+// if (document.getElementById("texttextttt").innerHTML == ' ') {
+//         document.getElementById("textsend").click();
+//
+// }
+// document.getElementById("textsend").click();
+//
+//
+//
+DB_HOST : 'd1403-6-sjp-service'
+DB_PORT : 'django.db.backends.mysql'
+DB_DATABASE : 'd1403-6qbg_db'
+DB_USERNAME : 'root'
+DB_PASSWORD :'krOenXvXrTN8WwUpr57q'
 
-            function setIndex() {
-                const i = this.style.getPropertyValue('--i');
-                underLine.style.setProperty('--i', i);
-                content.style.setProperty('--i', i);
-            }
+process.env.DB_HOST
+var mysql = require ('mysql');
 
-            radioList.forEach((item) =>
-                item.addEventListener('click', setIndex))
-
-            let back = document.querySelector('.back');
-            let chatBox = document.querySelector('.chatBox');
-            let open = document.querySelector('.open');
-
-            back.onclick = function() {
-                chatBox.classList.add('hide');
-            }
-
-            open.onclick = function() {
-                chatBox.classList.remove('hide');
-            }
+var con = mysql.createConnection({
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'd1403-6qbg_db',
+    'USER': 'root',
+    'PASSWORD': 'krOenXvXrTN8WwUpr57q',
+    'HOST': 'd1403-6-sjp-service',
+});
+con.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
