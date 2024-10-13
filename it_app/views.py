c10c76115b9f053335bf26de1a89fe03a9452ad3
Asus <ma4301511@gmail.com>
2024-10-13 23:14:36 +0330

    node test

--- it_app/views.py
+++ it_app/views.py
@@ -454,8 +454,8 @@ def tim(x):
             except:
                 print("not net yadavari")
         # Schedule the message to be sent at midnight
-        # schedule.every().day.at("08:30").do(yadavari_vaghtfarda)
-        schedule.every(20).seconds.do(yadavari_vaghtfarda)
+        schedule.every().day.at("08:30").do(yadavari_vaghtfarda)
+        # schedule.every(20).seconds.do(yadavari_vaghtfarda)
         while True:
             schedule.run_pending()
             time.sleep(1)
