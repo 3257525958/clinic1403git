e5dde8c0e8d274133fbda5d4aa757d6bd4e51d9d
Asus <ma4301511@gmail.com>
2024-12-22 01:43:21 +0330

    525

--- cantact_app/models.py
+++ cantact_app/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class accuntmodel(models.Model):
-    firstname = models.CharField(max_length=100 )
-    lastname = models.CharField(max_length=100 )
-    # melicode = models.CharField(max_length=15 , default='0')
-    # phonnumber = models.CharField(max_length=11 )
-    # savesabt = models.CharField(max_length=100)
-    # pasword = models.CharField(max_length=100)
-    # level = models.CharField(max_length=50,default='دسترسی معمولی')
-    # dayb = models.CharField(max_length=3 , default='0')
-    # mountb = models.CharField(max_length=20 , default='0')
-    # yearb = models.CharField(max_length=5, default='0')
+    firstname = models.CharField(max_length=100 ,null=True)
+    lastname = models.CharField(max_length=100 ,null=True)
+    melicode = models.CharField(max_length=15 , default='0',null=True)
+    phonnumber = models.CharField(max_length=11 ,null=True)
+    savesabt = models.CharField(max_length=100,null=True)
+    pasword = models.CharField(max_length=100,null=True)
+    level = models.CharField(max_length=50,default='دسترسی معمولی' ,null=True)
+    dayb = models.CharField(max_length=3 , default='0',null=True)
+    mountb = models.CharField(max_length=20 , default='0',null=True)
+    yearb = models.CharField(max_length=5, default='0',null=True)
 
     def __str__(self):
         return f"{self.melicode}"
