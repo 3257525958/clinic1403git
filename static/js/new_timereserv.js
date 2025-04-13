28965bf913899e312f0a2d94280a7a603c709b8e
Asus <ma4301511@gmail.com>
2025-04-13 23:33:35 +0330

    تا سر انتخاب تایم

--- static/js/new_timereserv.js
+++ static/js/new_timereserv.js
@@ -79,8 +79,8 @@ function generateTimeSlots(date) {
 // ارسال شماره روز به سرور با متد POST
 function sendDateToBackend(dayIndex) {
   console.log("Sending day index:", dayIndex);
-  // fetch('http://localhost:8000/reserv/new_timereserv/', {
-  fetch('http://drmahdiasadpour.ir/reserv/new_timereserv/', {
+  fetch('/reserv/new_timereserv/', {
+  // fetch('http://drmahdiasadpour.ir/reserv/new_timereserv/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -112,8 +112,8 @@ function sendDateToBackend(dayIndex) {
 
 // ارسال شماره تایم به همراه شماره روز به سرور با متد POST
 function sendTimeToBackend(dayIndex, timeNumber) {
-  // fetch('http://localhost:8000/reserv/new_timereserv/', {
-  fetch('http://drmahdiasadpour.ir/reserv/new_timereserv/', {
+  fetch('http://localhost:8000/reserv/new_timereserv/', {
+  // fetch('http://drmahdiasadpour.ir/reserv/new_timereserv/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
