442e5d880f884538d2b8fae81873164165819be4
Asus <ma4301511@gmail.com>
2025-07-12 00:51:21 +0330

    llllllllllbbbb

--- cash_app/views.py
+++ cash_app/views.py
@@ -31,6 +31,7 @@ ZIB_API_TOKEN = 'https://gateway.zibal.ir/v1/verify'
 MERCHANT_ZIBAL = 'zibal'
 # ENDURL = "http://127.0.0.1:8000"
 
+
 ENDURL = "https://drmahdiasadpour.ir"
 CALLBACK_ZIBAL_URL = 'https://drmahdiasadpour.ir/zib/verifyzibal/'
 # MERCHANT_ZIBAL = '64c2047fcbbc270017f4c6b2'
