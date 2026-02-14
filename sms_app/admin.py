dd456be6d9978292039fc4d5ac97f7a0d28b92d9
Asus <ma4301511@gmail.com>
2026-02-14 22:36:44 +0330

    start543

--- sms_app/admin.py
+++ sms_app/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from .models import MessageTemplate, SentMessage, ReceivedMessage
 
-# Register your models here.
+@admin.register(MessageTemplate)
+class MessageTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'text', 'created_at', 'updated_at')
+    search_fields = ('name', 'text')
 
+@admin.register(SentMessage)
+class SentMessageAdmin(admin.ModelAdmin):
+    list_display = ('contact', 'template', 'message_text', 'sent_at', 'status')
+    list_filter = ('status', 'template', 'sent_at')
+    search_fields = ('contact__firstname', 'contact__lastname', 'contact__phonnumber', 'message_text')
+    date_hierarchy = 'sent_at'
+
+@admin.register(ReceivedMessage)
+class ReceivedMessageAdmin(admin.ModelAdmin):
+    list_display = ('sender_number', 'contact', 'message_text', 'received_at', 'is_read')
+    list_filter = ('is_read', 'received_at')
+    search_fields = ('sender_number', 'contact__firstname', 'contact__lastname', 'message_text')
+    date_hierarchy = 'received_at'
